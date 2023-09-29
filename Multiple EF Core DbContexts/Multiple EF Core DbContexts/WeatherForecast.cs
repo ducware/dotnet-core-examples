6902349a66bf06c4c18a7e29cfec6238d84fdac4
Đặng Minh Đức <ducware@gmail.com>
2023-09-29 09:41:48 +0700

    init

---
+++ Multiple EF Core DbContexts/Multiple EF Core DbContexts/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Multiple_EF_Core_DbContexts
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
