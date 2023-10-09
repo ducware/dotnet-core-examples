11a1b633a1c785c8cb6cd7b6a1f7d43e4ae8c1dd
Đặng Minh Đức <ducware@gmail.com>
2023-10-09 17:17:54 +0700

    identity

---
+++ Identity DotNet 8/Identity DotNet 8/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_DotNet_8.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
