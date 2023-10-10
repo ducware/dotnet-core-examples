c92ba1b46ab1b0e2dd1d93c8ccf8482361b5b2f1
Đặng Minh Đức <ducware@gmail.com>
2023-10-10 14:12:43 +0700

    nuget

---
+++ NugetPackgeExample/MyLibrary.cs
@@ -0,0 +1,26 @@
+﻿namespace Ducware.Example;
+
+public static class ConvertNumberToText
+{
+    public static string ConvertPhoneNumber(string phoneNumber) 
+    {
+        // Mảng chuỗi tương ứng với từng chữ số
+        string[] numberText = { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+        
+        // Chuỗi kết quả
+        string result = "";
+        
+        // Duyệt qua từng chữ số trong số điện thoại
+        foreach (char digit in phoneNumber)
+        {
+            // Chuyển đổi chữ số thành số nguyên
+            int number = int.Parse(digit.ToString());
+
+            // Thêm chuỗi tương ứng vào kết quả
+            result += numberText[number] + " ";
+        }
+        
+        // Trả về kết quả, loại bỏ dấu cách ở cuối chuỗi
+        return result.TrimEnd();
+    }
+}
