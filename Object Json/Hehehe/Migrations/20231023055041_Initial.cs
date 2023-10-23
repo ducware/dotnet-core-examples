499179547ef7239b99f21234608abfcadcfa93df
Đặng Minh Đức <ducware@gmail.com>
2023-10-23 15:38:22 +0700

    feat: use object to json

---
+++ Object Json/Hehehe/Migrations/20231023055041_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hehehe.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expiration",
+                columns: table => new
+                {
+                    MomentType = table.Column<int>(type: "int", nullable: false),
+                    ExactMoment = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plans",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlanName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpirationJson = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plans", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expiration");
+
+            migrationBuilder.DropTable(
+                name: "Plans");
+        }
+    }
+}
---
+++ Object Json/Hehehe/ViewModels/PlanDto.cs
@@ -0,0 +1,16 @@
+﻿using Hehehe.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Hehehe.ViewModel
+{
+    public class PlanDto
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+        [JsonPropertyName("plan_name")]
+        public string? PlanName { get; set; }
+        [JsonPropertyName("expiration_json")]
+        public string? ExpirationJson { get; set; }
+    }
+}
