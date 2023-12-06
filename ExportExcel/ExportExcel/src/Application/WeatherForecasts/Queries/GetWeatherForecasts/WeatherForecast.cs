115bce7288561a479ccb5831aa406cd625951fe4
Đặng Minh Đức <ducware@gmail.com>
2023-12-06 14:46:31 +0700

    refactor: attribute

--- ExportExcel/ExportExcel/src/Application/WeatherForecasts/Queries/GetWeatherForecasts/WeatherForecast.cs
+++ ExportExcel/ExportExcel/src/Application/WeatherForecasts/Queries/GetWeatherForecasts/WeatherForecast.cs
@@ -3,12 +3,20 @@
 namespace ExportExcel.Application.WeatherForecasts.Queries.GetWeatherForecasts;
 public class WeatherForecast
 {
-    [ExcelColumn(Name = "Ngày", Index=0, Width = 100)]
+    [ExcelColumnIndex(0)]
+    [ExcelColumnName("Ngày")]
+    [ExcelColumnWidth(100)]
     public DateTime Date { get; init; }
-    [ExcelColumn(Name = "Độ C", Index = 1, Width = 45)]
+    [ExcelColumnIndex(1)]
+    [ExcelColumnName("Độ C")]
+    [ExcelColumnWidth(45)]
     public int TemperatureC { get; init; }
-    [ExcelColumn(Name = "Độ F", Index = 2, Width = 45)]
+    [ExcelColumnIndex(2)]
+    [ExcelColumnName("Độ F")]
+    [ExcelColumnWidth(45)]
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    [ExcelColumn(Name = "Tóm tắt", Index = 3, Width = 45)]
+    [ExcelColumnIndex(3)]
+    [ExcelColumnName("Tóm tắt")]
+    [ExcelColumnWidth(45)]
     public string? Summary { get; init; }
 }
